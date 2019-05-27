f3d8083372ba1176de5b6ab5e8467bd4b3a97dd8
llSyntaxErrorrll <38817473+llsyntaxerrorrll@users.noreply.github.com>
2019-05-27 16:39:06 -0500

    cambios en la calse java de la actividad mmain y su layout

--- app/src/main/java/com/apps/memorizame/Main.java
+++ app/src/main/java/com/apps/memorizame/Main.java
@@ -3,12 +3,12 @@ package com.apps.memorizame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
 
         //asdasdsd
     }

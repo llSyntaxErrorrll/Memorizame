7c0bab4f047a73c801468ceb0f10dced0c007a6b
llSyntaxErrorrll <38817473+llsyntaxerrorrll@users.noreply.github.com>
2019-05-28 01:06:24 -0500

    Eliminacion de los mipmpa de round modificaicon de values con dimens, string, y styles modificacion del gradle añadiendo piccaso creacion de la actividad de inicio, actividad basica creacion del splashscreen modificacion del manifest, permisos y actividad main

--- app/src/main/java/com/apps/memorizame/Main.java
+++ app/src/main/java/com/apps/memorizame/Main.java
@@ -1,15 +1,41 @@
 package com.apps.memorizame;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //poner la pantalla completa
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        //asdasdsd
+        //iniciar la tarea
+        splashScreen screen = new splashScreen();
+        screen.execute();
+    }
+
+    //tarea asincronica para el splascreen
+    public class splashScreen extends AsyncTask{
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            try{
+                //1.5s y pasar de actividad
+                Thread.sleep(1500);
+                Intent pasar = new Intent(Main.this, Inicio.class);
+                startActivity(pasar);
+                finish();
+            }catch (Exception e){
+                e.notify();
+            }
+            return null;
+        }
     }
 }

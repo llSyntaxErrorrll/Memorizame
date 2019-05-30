7f7ed50bf9772c93c909731ecb61d883626b0223
Jonathan Mosquera Alban <45472682+jonathanwick10@users.noreply.github.com>
2019-05-30 07:46:01 -0500

    se modifica la clase java preguntas

--- app/src/main/java/com/apps/memorizame/Preguntas.java
+++ app/src/main/java/com/apps/memorizame/Preguntas.java
@@ -1,11 +1,17 @@
 package com.apps.memorizame;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.apps.memorizame.Entitys.PreguntasEntity;
 import com.apps.memorizame.SQLite.PreguntasCRUD;
@@ -19,8 +25,10 @@ public class Preguntas extends AppCompatActivity {
     RadioButton radioButton1, radioButton2, radioButton3, radioButton4, radioButton5,
     radioButton6,radioButton7, radioButton8, radioButton9, radioButton10, radioButton11,
     radioButton12,radioButton13, radioButton14, radioButton15, radioButton16, radioButton17, radioButton18;
-    TextView txtPregunta1,txtPregunta2, txtPregunta3, txtPregunta4, txtPregunta5, txtPregunta6;
+    TextView txtPregunta1,txtPregunta2, txtPregunta3, txtPregunta4, txtPregunta5, txtPregunta6, txttotal;
+    Button btnTermionarPregunta;
     int idSubCategoria;
+    String respuestCorecct1,respuestCorecct2,respuestCorecct3,respuestCorecct4,respuestCorecct5,respuestCorecct6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,8 +66,13 @@ public class Preguntas extends AppCompatActivity {
         txtPregunta5.setText(entityArrayList.get(4).getNombrePregunta());
         txtPregunta6.setText(entityArrayList.get(5).getNombrePregunta());
 
-
-
+        //respuestas
+        respuestCorecct1 = entityArrayList.get(0).getRespuestaCorrect();
+        respuestCorecct2 = entityArrayList.get(1).getRespuestaCorrect();
+        respuestCorecct3 = entityArrayList.get(2).getRespuestaCorrect();
+        respuestCorecct4 = entityArrayList.get(3).getRespuestaCorrect();
+        respuestCorecct5 = entityArrayList.get(4).getRespuestaCorrect();
+        respuestCorecct6 = entityArrayList.get(5).getRespuestaCorrect();
 
         //setear  el valor a los radioButton
         radioButton1.setText(entityArrayList.get(0).getRespuesta1());
@@ -85,8 +98,150 @@ public class Preguntas extends AppCompatActivity {
         radioButton16.setText(entityArrayList.get(5).getRespuesta1());
         radioButton17.setText(entityArrayList.get(5).getRespuesta2());
         radioButton18.setText(entityArrayList.get(5).getRespuesta3());
+
+        respuestasCorrectas();
+        //boton
+
     }
 
+    private void rdioButtonRojos() {
+
+        radioButton1.setTextColor(getResources().getColor(R.color.RojoOscuro));
+        radioButton2.setTextColor(getResources().getColor(R.color.RojoOscuro));
+        radioButton3.setTextColor(getResources().getColor(R.color.RojoOscuro));
+        radioButton4.setTextColor(getResources().getColor(R.color.RojoOscuro));
+        radioButton5.setTextColor(getResources().getColor(R.color.RojoOscuro));
+        radioButton6.setTextColor(getResources().getColor(R.color.RojoOscuro));
+        radioButton7.setTextColor(getResources().getColor(R.color.RojoOscuro));
+        radioButton8.setTextColor(getResources().getColor(R.color.RojoOscuro));
+        radioButton9.setTextColor(getResources().getColor(R.color.RojoOscuro));
+        radioButton10.setTextColor(getResources().getColor(R.color.RojoOscuro));
+        radioButton11.setTextColor(getResources().getColor(R.color.RojoOscuro));
+        radioButton12.setTextColor(getResources().getColor(R.color.RojoOscuro));
+        radioButton13.setTextColor(getResources().getColor(R.color.RojoOscuro));
+        radioButton14.setTextColor(getResources().getColor(R.color.RojoOscuro));
+        radioButton15.setTextColor(getResources().getColor(R.color.RojoOscuro));
+        radioButton16.setTextColor(getResources().getColor(R.color.RojoOscuro));
+        radioButton17.setTextColor(getResources().getColor(R.color.RojoOscuro));
+        radioButton18.setTextColor(getResources().getColor(R.color.RojoOscuro));
+    }
+
+    private void respuestasCorrectas() {
+
+        btnTermionarPregunta.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (radioGroup1.getCheckedRadioButtonId() == -1){
+
+                }else if (radioGroup2.getCheckedRadioButtonId() == -1){
+
+                }else if (radioGroup3.getCheckedRadioButtonId() == -1){
+
+                }else if(radioGroup4.getCheckedRadioButtonId() == -1){
+
+                }else if(radioGroup5.getCheckedRadioButtonId() == -1){
+
+                }else if(radioGroup6.getCheckedRadioButtonId() == -1) {
+                    // no radio buttons are checked
+                } else {
+
+                    int total = 0;
+                    rdioButtonRojos();
+
+                // one of the radio buttons is checked
+                    int radioButtonId = radioGroup1.getCheckedRadioButtonId();
+                    View radioButton = radioGroup1.findViewById(radioButtonId);
+                    int indice = radioGroup1.indexOfChild(radioButton);
+                    RadioButton rb = (RadioButton)  radioGroup1.getChildAt(indice);
+                    String texto = rb.getText().toString();
+                    if (texto == texto){
+                        rb.setTextColor(getResources().getColor(R.color.VerdeClaroOscuro));
+                        total += 1;
+                    }
+
+                    int radioButtonId2 = radioGroup2.getCheckedRadioButtonId();
+                    View radioButton2 = radioGroup2.findViewById(radioButtonId2);
+                    int indice2 = radioGroup2.indexOfChild(radioButton2);
+                    RadioButton rb2 = (RadioButton)  radioGroup2.getChildAt(indice2);
+                    String texto2 = rb2.getText().toString();
+                    if (texto2 == texto2){
+                        rb2.setTextColor(getResources().getColor(R.color.VerdeClaroOscuro));
+                        total += 1;
+                    }
+
+                    int radioButtonId3 = radioGroup3.getCheckedRadioButtonId();
+                    View radioButton3 = radioGroup3.findViewById(radioButtonId3);
+                    int indice3 = radioGroup3.indexOfChild(radioButton3);
+                    RadioButton rb3 = (RadioButton)  radioGroup1.getChildAt(indice3);
+                    String texto3 = rb3.getText().toString();
+                    if (texto3 == texto3){
+                        rb3.setTextColor(getResources().getColor(R.color.VerdeClaroOscuro));
+                        total += 1;
+                    }
+
+                    int radioButtonId4 = radioGroup4.getCheckedRadioButtonId();
+                    View radioButton4 = radioGroup4.findViewById(radioButtonId4);
+                    int indice4= radioGroup4.indexOfChild(radioButton4);
+                    RadioButton rb4 = (RadioButton)  radioGroup4.getChildAt(indice4);
+                    String texto4 = rb4.getText().toString();
+                    if (texto4 == texto4){
+                        rb4.setTextColor(getResources().getColor(R.color.VerdeClaroOscuro));
+                        total += 1;
+                    }
+
+                    int radioButtonId5 = radioGroup5.getCheckedRadioButtonId();
+                    View radioButton5 = radioGroup5.findViewById(radioButtonId5);
+                    int indice5 = radioGroup5.indexOfChild(radioButton5);
+                    RadioButton rb5 = (RadioButton)  radioGroup5.getChildAt(indice5);
+                    String texto5 = rb5.getText().toString();
+                    if (texto5 == texto5){
+                        rb5.setTextColor(getResources().getColor(R.color.VerdeClaroOscuro));
+                        total += 1;
+                    }
+
+                    int radioButtonId6 = radioGroup6.getCheckedRadioButtonId();
+                    View radioButton6 = radioGroup6.findViewById(radioButtonId6);
+                    int indice6 = radioGroup6.indexOfChild(radioButton6);
+                    RadioButton rb6 = (RadioButton)  radioGroup6.getChildAt(indice6);
+                    String texto6 = rb6.getText().toString();
+                    if (texto6 == texto6){
+                        rb6.setTextColor(getResources().getColor(R.color.VerdeClaroOscuro));
+                        total += 1;
+                    }
+                    String tot = String.valueOf(total);
+                    txttotal.setText(tot+"/6");
+                    AlertDialog.Builder dialogo1 = new AlertDialog.Builder(Preguntas.this);
+                    dialogo1.setTitle("Genial!!!");
+                    dialogo1.setMessage("        Tu puntaje es de "+tot+"/6");
+                    dialogo1.setCancelable(false);
+                    dialogo1.setPositiveButton("Continuar", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialogo1, int id) {
+                            aceptar();
+                        }
+                    });
+                    dialogo1.setNegativeButton("Reintentar", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialogo1, int id) {
+                            cancelar();
+                        }
+                    });
+                    dialogo1.show();
+                }
+            }
+        });
+    }
+
+    private void aceptar(){
+
+        Intent inte = new Intent(Preguntas.this, Inicio.class);
+        inte.putExtra(Constans.dbColumSubCatego_id,idSubCategoria);
+        startActivity(inte);
+    }
+    private void cancelar(){
+        Intent inte = new Intent(Preguntas.this, MemorizaImagen.class);
+        inte.putExtra(Constans.dbColumSubCatego_id,idSubCategoria);
+        startActivity(inte);
+    }
     private void referenciar() {
         radioGroup1 = (RadioGroup) findViewById(R.id.rgUno);
         radioGroup2 = (RadioGroup) findViewById(R.id.rgDos);
@@ -101,6 +256,7 @@ public class Preguntas extends AppCompatActivity {
         txtPregunta4 = (TextView) findViewById(R.id.txtPreguntaCuatro);
         txtPregunta5 = (TextView) findViewById(R.id.txtPreguntaCinco);
         txtPregunta6 = (TextView) findViewById(R.id.txtPreguntaSeis);
+        txttotal = (TextView) findViewById(R.id.txtTotal);
         //radio Button
         radioButton1 = (RadioButton) findViewById(R.id.rbUno);
         radioButton2 = (RadioButton) findViewById(R.id.rbDos);
@@ -120,5 +276,7 @@ public class Preguntas extends AppCompatActivity {
         radioButton16 = (RadioButton) findViewById(R.id.rbDiezySeis);
         radioButton17 = (RadioButton) findViewById(R.id.rbDiezysiete);
         radioButton18 = (RadioButton) findViewById(R.id.rbDiezyocho);
+        //botones
+        btnTermionarPregunta = (Button) findViewById(R.id.btnterminarPregu);
     }
 }
